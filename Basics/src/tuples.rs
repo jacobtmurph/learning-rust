ea68a2eb24ee1557c6b5b0eca116480714b6848d
Jacob T. Murphy <jacobtmurphy@fastmail.com>
2023-04-10 13:23:16 +0100

    Update tuples.rs w/ example

--- Basics/src/tuples.rs
+++ Basics/src/tuples.rs
@@ -1,3 +1,7 @@
+// Tuples group tofwther a maximum of 12 values of different types
+
 pub fn run() {
-    
+    let person: (&str, &str, i8) = ("Jacob", "Norther Ireland", 24);
+
+    println!("{} is from {} and is {}.", person.0, person.1, person.2);
 }
